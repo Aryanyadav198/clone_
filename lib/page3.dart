da252e4106602e1394d73428d09836499e09c327
Aryan yadav <aryanyadav898487@gmail.com>
2024-12-12 18:14:27 +0600

    Adding Container

--- lib/page3.dart
+++ lib/page3.dart
@@ -6,35 +6,45 @@ class Page3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [IconButton(onPressed: (){Scaffold.of(context).openDrawer();}, icon: const Icon(Icons.menu))],
-        title: const Text("Drawer"),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text("Drawer Header"),
-            ),
-            ListTile(
-              title: const Text("Aryan Yadav"),
-              onTap: () {},
-            ),
-            ListTile(
-              onTap: () {},
-              title: const Text("aryanyadav898487@gmail.com"),
-            )
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                icon: const Icon(Icons.menu))
           ],
+          title: const Text("Drawer"),
         ),
-      ),
-      body: const Center(
-        child: Text(
-          "You have done a wrong thing  be ready for Punishment ",
-          style: TextStyle(
-              height: 24, color: Colors.red, fontWeight: FontWeight.bold),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text("Drawer Header"),
+              ),
+              ListTile(
+                title: const Text("Aryan Yadav"),
+                onTap: () {},
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text("aryanyadav898487@gmail.com"),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20)),
+            ),
+          ),
+        ));
   }
 }
